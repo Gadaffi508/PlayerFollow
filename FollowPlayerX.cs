b4cfa8392a44570ac9159059a9153e54476cba83
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2023-03-06 11:24:57 +0300

    PlayerFollow
    Follow

---
+++ FollowPlayerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3((float)19.09785, (float)2.731182, (float)-2.237558);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
